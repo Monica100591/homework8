1b871dc16c16c01e12e5217b68b696133409dd79
Alber Moied <am1545@wildcats.unh.edu>
2019-04-01 04:38:08 -0400

    runSimulation method incomplete

--- src/PublisherImplementation.java
+++ src/PublisherImplementation.java
@@ -19,7 +19,32 @@ public class PublisherImplementation implements PublisherInterface {
 	}
 	
 	public void runSimulation () {
+		int oddVal = 0;
+		int threeVal = 0;
+		boolean istrue = false;
 		
+		for (int i = 1; i <= 200; i++) {
+			Event ev = new Event(i, new Random().nextInt(5000) + 1);
+			notifyObserver(ev);
+			for (Observer ob : subscribers) {
+				istrue = ob.notifyObserver(ev);
+				if (ob instanceof SubscriberOdds && istrue) {
+					oddVal += 1;
+					if (oddVal > 20) {
+						removeObserver(ob);
+					}
+				} else if (ob instanceof SubscriberThrees) {
+					threeVal += 1;
+					if (threeVal > 6) {
+						removeObserver(ob);
+					} 
+				}
+			}
+			if ( i % 40 == 0 && i / 40 > 0 && i / 40 < 6) {	// For iteration 40, 80, 120, 160, 200
+				// register SubscriberOdds if it is unregistered
+				// register SubscriberThrees if it is unregistered
+			}
+		}
 	}
 	
 	public void registerObserver(Observer o){
