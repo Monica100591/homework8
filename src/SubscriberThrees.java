8342dbf2762b362bc7a6327c403532c8cfcd5f44
Alber Moied <am1545@wildcats.unh.edu>
2019-04-01 01:28:31 -0400

    implemented SubscriberThrees class

---
+++ src/SubscriberThrees.java
@@ -0,0 +1,13 @@
+
+public class SubscriberThrees implements Observer {
+
+	@Override
+	public boolean notifyObserver(Event e) {
+		if(e.getEventData() % 3 == 0) {
+		    System.out.println("Event " + e.getEventNumber() +  "is divisible by 3: " + e.getEventData());
+		    return true;
+		} else {
+			return false;
+		}		
+	}
+ }
