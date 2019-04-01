e43d940d2f78ced0ab26c8e360e1b62c54d9b9eb
Alber Moied <am1545@wildcats.unh.edu>
2019-04-01 01:25:41 -0400

    implemented the main method

--- src/RunHW8.java
+++ src/RunHW8.java
@@ -9,10 +9,19 @@
  */
 public class RunHW8 {
 	
-	private PublisherImplementation pub;
+	private static PublisherImplementation pub;
 	
 	public static void main (String args[]) {
+		pub = new PublisherImplementation();
 		
+		SubscriberOdds odds = new SubscriberOdds();
+		SubscriberEvens evens = new SubscriberEvens();
+		SubscriberThrees threes = new SubscriberThrees();
+		
+		pub.registerObserver(odds);
+		pub.registerObserver(evens);
+		pub.registerObserver(threes);
+		pub.runSimulation();
 	}
 	
 }
