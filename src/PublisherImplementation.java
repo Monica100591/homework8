15284d166bafcb68ec4374648056986543689841
Alber Moied <am1545@wildcats.unh.edu>
2019-04-01 00:51:39 -0400

    runSimulation method not implemented yet

--- src/PublisherImplementation.java
+++ src/PublisherImplementation.java
@@ -1,20 +1,37 @@
 /**
  * 
  */
-
+import java.util.*;
 /**
  * @author monic_000
  *
  */
-public class PublisherImplementation implements Publisher {
-	private List<Observer> subscribers = new ArrayList<>(); 
+public class PublisherImplementation implements PublisherInterface {
+	private List<Observer> subscribers = new ArrayList<Observer>(); 
 	
-	private Event generateEvent () {
+	private Event generateEvent() {
+		return new Event();
 	} 
 	
 	public void notifyObserver (Event e) {
+		for(Observer sub: subscribers)
+			sub.notifyObserver(e);
 	}
 	
 	public void runSimulation () {
+		
+	}
+	
+	public void registerObserver(Observer o){
+		subscribers.add(o);
+	}
+	
+	public void removeObserver(Observer o) {
+		subscribers.remove(o);
+	}
+	
+	@Override
+	public void notifyObserver (Observer o) {
+		o.notifyObserver(generateEvent());
 	}
 }
